db26d83e5d8165e3671be1cc2ea8967436cdfd1e
Ivo <admin@vps2251.directvps.nl>
2013-07-31 17:33:57 +0200

    Orientation and home button working fine again.

--- src/com/ivolek/torch/MainActivity.java
+++ src/com/ivolek/torch/MainActivity.java
@@ -1,32 +1,29 @@
 package com.ivolek.torch;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.admin.DevicePolicyManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.hardware.Camera;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity 
 {
 
-	Camera camera;
-	Boolean afsluiter = true;
 	@Override
 	public void onCreate(Bundle savedInstanceState) 
 	{
 		final MainActivity me = this;
 		Intent service = new Intent(me, FlashlightService.class);
-		me.startService(service); 		
-		
+		me.startService(service); 
 		setContentView(R.layout.activity_main);
-//		camera = Camera.open();
-//		Parameters p = camera.getParameters();
-//		p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-//		camera.setParameters(p);
-//		camera.startPreview();
 		super.onCreate(savedInstanceState);
 		//getWindow().addFlags(PowerManager.PARTIAL_WAKE_LOCK);
 		//Scherm aanblijven.
@@ -35,62 +32,74 @@ public class MainActivity extends Activity
 		WindowManager.LayoutParams layout = getWindow().getAttributes();
 		layout.screenBrightness = -1;
 		getWindow().setAttributes(layout);
-		new Thread()
-		{
-			public void run()
-			{
-				try
-				{
-					Thread.sleep(600000);
-				}
-				catch(Exception e){}
-				lockDevice();
-				Intent service = new Intent(me, FlashlightService.class);
-				me.stopService(service); 
-				System.exit(0);
-			}
-		}.start();
-
-
+		
+		registerReceiver(destroyReceiver, new IntentFilter("destroy"));
 	}
+
+	private final BroadcastReceiver destroyReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			lockDevice();
+			unregisterReceiver(this);
+			finish();   
+		}
+	};
 
 	@Override
 	public void onBackPressed() 
 	{
 		// TODO Auto-generated method stub;
-		super.onBackPressed();
+		super.onBackPressed();		
+		try{
+			unregisterReceiver(destroyReceiver);}
+		catch(Exception e){}
+		lockDevice(); 
+	}
+	
+	@Override
+	protected void onStop() 
+	{
+		Log.i("TAG", "KKKKKKKKKKKK");
 
-		lockDevice();
-		Intent service = new Intent(this, FlashlightService.class);
-		this.stopService(service); 
+		super.onStop();
+		try{
+			unregisterReceiver(destroyReceiver);}
+		catch(Exception e){}		
+		ActivityManager am = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+
+		List< ActivityManager.RunningTaskInfo > taskInfo = am.getRunningTasks(1); 
+		if (!taskInfo.get(0).topActivity.getPackageName().equals("com.ivolek.torch")){
+//			Intent service = new Intent(this, FlashlightService.class);
+//			this.stopService(service); 
+//			finish();
+			lockDevice();
+		}
 	}
 
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-
-//		lockDevice();
-		Intent service = new Intent(this, FlashlightService.class);
-		this.stopService(service); 
-	}   
-	
 	protected static final int REQUEST_ENABLE = 0;
 	DevicePolicyManager devicePolicyManager;
+
 	ComponentName adminComponent;
 	public  void lockDevice()
 	{
+		try{
+			unregisterReceiver(destroyReceiver);}
+		catch(Exception e){}
+		
+		Intent service = new Intent(this, FlashlightService.class);
+		this.stopService(service); 
+		
 		adminComponent = new ComponentName(MainActivity.this, Darclass.class);
 		devicePolicyManager = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
-		
-         if (!devicePolicyManager.isAdminActive(adminComponent)) {
 
-             Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-             intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminComponent);
-             startActivityForResult(intent, REQUEST_ENABLE);
-         } else {
-		devicePolicyManager.lockNow();
+		if (!devicePolicyManager.isAdminActive(adminComponent)) {
+
+			Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+			intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, adminComponent);
+			startActivityForResult(intent, REQUEST_ENABLE);
+		} else {
+			devicePolicyManager.lockNow();
 		}
-
+		finish();
 	}
 }
